961f34ed9e0f570a8340cbd016e70506365d0f7d
Keivan Mojmali <mojmalik@gmail.com>
2020-11-16 13:16:23 -0800

    Javascript-primitives-and-variables | Keivan Mojmali

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+var fullName = "Keivan Mojmali";
+var isCool = true;
+var totalPets = 1;
+var unknown = null;
+var never;
+
+
+
+console.log('Value of fullName: '+fullName);
+console.log('Value of isCool: '+ isCool);
+console.log('Value of totalPets: ' + totalPets);
+
+
+
+console.log('typeOf of fullName: ' + typeof fullName);
+console.log('typeOf of isCool: ' + typeof isCool);
+console.log('typeOf of totalPets: ' + typeof totalPets);
+
+
+console.log('Value of unknown: '+ unknown);
+console.log('typeOf unknown: ' + typeof unknown);
+
+console.log('Value of never: ' + never);
+console.log('typeOf never: ' + never);
