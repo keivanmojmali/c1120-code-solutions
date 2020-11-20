b73cdcbfdb26aca5f2400a0a3ef09e68a7fbf2b6
Keivan Mojmali <mojmalik@gmail.com>
2020-11-19 16:38:18 -0800

    updating

--- javascript-loops-practice/includes-seven.js
+++ javascript-loops-practice/includes-seven.js
@@ -1,1 +1,12 @@
 /* exported includesSeven */
+
+function includesSeven(array) {
+  var seven = [];
+  for (i = 0; i < array.length; i++) {
+    if(array[i] === 7) {
+      seven.push(true);
+    } else {
+      return false
+    }
+  }
+}
