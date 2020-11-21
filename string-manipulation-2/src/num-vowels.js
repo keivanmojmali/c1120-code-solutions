0a3075d662f8db575d382ee2897f20d47e46f635
Keivan Mojmali <mojmalik@gmail.com>
2020-11-20 21:53:53 -0800

    half way

---
+++ string-manipulation-2/src/num-vowels.js
@@ -0,0 +1,14 @@
+/* exported numVowels */
+
+function numVowels(string) {
+  var number = [];
+  var nL = number.length
+  for(i = 0; i < string.length; i++) {
+    if(string[i] === 'A' || 'A') {
+      number.push(string[i]);
+    }
+  }
+  console.log(string[3]);
+  return nL;
+
+}
