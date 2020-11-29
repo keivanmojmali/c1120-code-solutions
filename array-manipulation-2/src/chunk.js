afcdbeba55ba7714392def957ad10962994c0617
Keivan Mojmali <mojmalik@gmail.com>
2020-11-29 15:11:39 -0800

    array-manipulation-2/src/chunk.js | one more to go

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,5 +1,12 @@
 /* exported chunk */
 
 function chunk(array,size) {
+  var newArray = [];
+  for(var i = 0; i < array.length;i = i + size) {
+    var count = size;
+    newArray.push(array.slice(array[i],count));
+    count = count + size;
 
+  }
+  return newArray;
 }
