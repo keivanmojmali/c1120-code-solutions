61861cb14cd9d1f6ac3b4063833d885ce653e18e
Keivan Mojmali <mojmalik@gmail.com>
2020-12-08 17:56:36 -0800

    javascript-constructors/ | Keivan Mojmali

--- javascript-constructors/student.js
+++ javascript-constructors/student.js
@@ -1,8 +1,23 @@
 /* exported Student */
 
 
-function Student(firstName,lastName,subject) {
+function Student(firstName, lastName, subject) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.subject = subject;
 }
+
+var studentPrototypes = {
+  getFullName: function () {
+    var getFullName = this.firstName + ' ' + this.lastName;
+    return getFullName;
+  },
+  getIntroduction: function () {
+    var introduction = 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
+    return introduction;
+  }
+
+}
+
+Student.prototype = studentPrototypes;
+var ron = new Student('Ron', 'Jeffries', 'extreme programming');
