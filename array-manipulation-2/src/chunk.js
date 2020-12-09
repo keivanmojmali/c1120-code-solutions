f6a9386d04ee2c48e1482db48fa0d89f4e9161b6
Keivan Mojmali <mojmalik@gmail.com>
2020-12-09 10:02:27 -0800

    still working on chunk

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,11 +2,11 @@
 
 function chunk(array,size) {
   var newArray = [];
-  for(var i = 0; i < array.length;i = i + size) {
-    var count = size;
-    newArray.push(array.slice(array[i],count));
-    count = count + size;
-
+  count = size;
+  for(var i = 2; i < array.length; i = i + size) {
+    var slice = array.slice(array[i],count);
+    newArray.push([slice]);
+    count += size;
   }
   return newArray;
 }
