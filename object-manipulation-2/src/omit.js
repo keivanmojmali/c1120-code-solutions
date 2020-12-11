47e36491c80e79bb424fa5f6c7faed208428ee9a
Keivan Mojmali <mojmalik@gmail.com>
2020-12-10 21:52:15 -0800

    working on it still

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,16 @@
 /* exported omit */
+
+function omit(source,keys) {
+  var source = source;
+  var final = {};
+  console.log(keys.length);
+  for(var i = 0; i < keys.length; i++) {
+    for(var key in source) {
+      if(key !== keys[i]) {
+        console.log('valueeeeee',source[key],key);
+        final[key] = source[key];
+      }
+    }
+  }
+  return final;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -2,18 +2,13 @@
 
 function pick(source, keys) {
   var source = source;
-  var props = [];
+  var props = {};
   for(var i = 0; i < keys.length; i++ ) {
     for(var key in source) {
-      if(key === keys[i]) {
-        props.push(key,source[key]);
+      if(key === keys[i] && source[key] !== undefined) {
+        props[key] = source[key];
       }
     }
   }
   return props;
 }
-
-
-/* this function almost works -
-return items needed but wiith a comma in the middle -
-so it does not get counted - need to return key and source[key] but together */
