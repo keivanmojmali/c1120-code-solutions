0d8f8ab86ce30c90cb0e94d9cdca16c16b18bb41
Keivan Mojmali <mojmalik@gmail.com>
2020-12-10 17:11:50 -0800

    still working on it

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,12 +1,19 @@
 /* exported pick */
 
 function pick(source, keys) {
- var hold = [source];
- for(var key in hold) {
-  if(key === keys) {
-    hold.push(key,value);
+  var source = source;
+  var props = [];
+  for(var i = 0; i < keys.length; i++ ) {
+    for(var key in source) {
+      if(key === keys[i]) {
+        props.push(key,source[key]);
+      }
+    }
   }
-  return hold;
- }
-
+  return props;
 }
+
+
+/* this function almost works -
+return items needed but wiith a comma in the middle -
+so it does not get counted - need to return key and source[key] but together */
