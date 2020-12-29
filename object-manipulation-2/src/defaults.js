449e4ac8d89cc8abbf2a613f301621b5cacb93c3
Keivan Mojmali <mojmalik@gmail.com>
2020-12-29 13:15:22 -0800

    fixing omit then submitting

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -2,22 +2,24 @@
 
 function defaults(target, source) {
   var hold = {};
-  var holdArray = [target];
-  console.log('length of hold array', holdArray.length);
-  if(holdArray.length === 0 ) {
-    Object.assign(target,source);
-  }
-  for (var start in source) {
-    for (var end in target) {
-      if (start !== end && source[start] !== target[end]) {
-        var sendTo = source[start];
-        hold[start] = sendTo;
+
+
+  if (Object.keys(target).length === 0) {
+    Object.assign(target, source);
+  } else {
+
+    for (var start in source) {
+      for (var end in target) {
+        if (start !== end && source[start] !== target[end]) {
+          var sendTo = source[start];
+          hold[start] = sendTo;
+        }
       }
     }
+    for (var final in target) {
+      var sendTo = target[final];
+      hold[final] = sendTo;
+    }
+    Object.assign(target, hold);
   }
-  for (var final in target) {
-    var sendTo = target[final];
-    hold[final] = sendTo;
-  }
-  Object.assign(target, hold);
 }
