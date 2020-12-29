a8aa81db33cfb22bbb28f30ab4910276cfc572c2
Keivan Mojmali <mojmalik@gmail.com>
2020-12-28 17:13:12 -0800

    working on invert

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,11 @@
 /* exported invert */
+
+function invert(source) {
+  var final = [];
+  for(var key in source) {
+    final.push(source[key], key);
+    console.log(source[key], key);
+
+  }
+  console.log('value of final',final);
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,16 +1,16 @@
 /* exported omit */
 
+
 function omit(source,keys) {
-  var source = source;
+  var original = source;
   var final = {};
-  console.log(keys.length);
   for(var i = 0; i < keys.length; i++) {
-    for(var key in source) {
-      if(key !== keys[i]) {
-        console.log('valueeeeee',source[key],key);
-        final[key] = source[key];
+    for(var key in original) {
+      if(key === keys[i]) {
+         delete original[key];
       }
     }
   }
+  final = original;
   return final;
 }
