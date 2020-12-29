c696c60836f1b2252d37f99105dcd0409289ade8
Keivan Mojmali <mojmalik@gmail.com>
2020-12-29 12:55:10 -0800

    default almost finished - wrking on submisison next

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,23 @@
 /* exported defaults */
+
+function defaults(target, source) {
+  var hold = {};
+  var holdArray = [target];
+  console.log('length of hold array', holdArray.length);
+  if(holdArray.length === 0 ) {
+    Object.assign(target,source);
+  }
+  for (var start in source) {
+    for (var end in target) {
+      if (start !== end && source[start] !== target[end]) {
+        var sendTo = source[start];
+        hold[start] = sendTo;
+      }
+    }
+  }
+  for (var final in target) {
+    var sendTo = target[final];
+    hold[final] = sendTo;
+  }
+  Object.assign(target, hold);
+}
