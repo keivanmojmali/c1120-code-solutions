03de0ee62488e11346e3dfb8bc1ed533fc515c00
Keivan Mojmali <mojmalik@gmail.com>
2020-12-30 13:22:08 -0800

    working on equal - 90 percent finished

---
+++ array-manipulation-3/src/equal.js
@@ -0,0 +1,10 @@
+/* exported equal */
+
+function equal(first,second) {
+  for(let i = 0; i < first.length; i++) {
+    if(first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
