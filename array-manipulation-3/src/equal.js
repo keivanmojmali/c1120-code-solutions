37661f6828a8291891f4bf9097ab69bfeb1c60cf
Keivan Mojmali <mojmalik@gmail.com>
2020-12-30 13:24:59 -0800

    working on flatten

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,6 +1,11 @@
 /* exported equal */
 
 function equal(first,second) {
+
+  if(first.length !== second.length) {
+    return false;
+  }
+
   for(let i = 0; i < first.length; i++) {
     if(first[i] !== second[i]) {
       return false;
