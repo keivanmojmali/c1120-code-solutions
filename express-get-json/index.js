babd36be6b7fd2e4beb7a9371da0f440baffab24
Keivan Mojmali <mojmalik@gmail.com>
2021-01-06 15:43:42 -0800

    express-get-json | Keivan Mojmali

---
+++ express-get-json/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades',(req,res) => {
+  const hold = [];
+  for(let key in grades) {
+    hold.push(grades[key]);
+  }
+  res.json(hold);
+
+
+})
+
+app.listen(3000, () => {
+
+})
