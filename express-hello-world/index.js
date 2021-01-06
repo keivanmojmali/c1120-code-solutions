ce5a450e5765b20133222cb51ed49a247b86c1c2
Keivan Mojmali <mojmalik@gmail.com>
2021-01-06 13:16:20 -0800

    express-hello-world | Keivan Mojmali

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 app.use((req,res) => {
-  console.log('method property of the req object', req.method);
+
   res.send('This is a string');
 })
 
 
 
 app.listen(3000, () => {
-  console.log('server is listening on port 3000');
+
 })
