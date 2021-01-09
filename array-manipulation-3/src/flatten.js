b58094ad2ec345e515a4440cabf3fe1f241836b2
Keivan Mojmali <mojmalik@gmail.com>
2021-01-08 16:04:22 -0800

    working on zip

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,2 +1,16 @@
 /* exported flatten */
-// take a look at reduce ()
+
+function flatten(array) {
+  const hold = [];
+  for(let i = 0; i < array.length; i++) {
+    let current = array[i];
+    if(typeof current === 'object') {
+      for(let key in current){
+        hold.push(current[key]);
+      }
+    } else {
+      hold.push(current);
+    }
+  }
+  return hold;
+}
