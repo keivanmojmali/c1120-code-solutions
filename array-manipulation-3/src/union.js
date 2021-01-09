f2ff2b371838332efec02fcbddc8769c87bf4d18
Keivan Mojmali <mojmalik@gmail.com>
2021-01-08 17:43:34 -0800

    array-manipulation-3 | Keivan Mojmali

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,5 +1,18 @@
 /* exported union */
 
-function union(first,second) {
+function union(first, second) {
+  const mainArray = [];
 
+  for(let i = 0; i < first.length; i++) {
+    if(mainArray.includes(first[i]) === false) {
+      mainArray.push(first[i]);
+    }
+  }
+
+  for(let u = 0; u < second.length; u++) {
+    if(mainArray.includes(second[u]) === false) {
+      mainArray.push(second[u]);
+    }
+  }
+  return mainArray;
 }
