c2011ddea7a26ab1f55a9d371388391fd845c26d
Keivan Mojmali <mojmalik@gmail.com>
2021-01-08 17:30:14 -0800

    working on intersection

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,16 @@
 /* exported difference */
+
+function difference(first,second) {
+  const mainArray = [];
+  for(let floop = 0; floop < first.length; floop++) {
+    if(second.includes(first[floop]) === false) {
+      mainArray.push(first[floop]);
+    }
+  }
+  for(let sloop = 0; sloop < second.length; sloop++) {
+    if(first.includes(second[sloop]) === false) {
+      mainArray.push(second[sloop]);
+    }
+  }
+  return mainArray;
+}
