94ee4321927d053a4586eff0ded0237318cbf271
Keivan Mojmali <mojmalik@gmail.com>
2021-01-08 17:06:17 -0800

    working on unique

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,22 @@
 /* exported zip */
+
+
+function zip(first,second) {
+  let shorter = 0;
+  const mainHold = [];
+
+  if(first.length > second.length) {
+    shorter = second.length;
+  } else {
+    shorter = first.length;
+  }
+
+  for(let i = 0; i < shorter; i++) {
+    let fHold = first[i];
+    let sHold = second[i];
+    mainHold.push([fHold,sHold]);
+
+
+  }
+  return mainHold;
+}
