ffa30c010924792a3e54528375833582fccd033d
Keivan Mojmali <mojmalik@gmail.com>
2021-01-08 17:16:47 -0800

    working on difference

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,12 @@
 /* exported unique */
+
+function unique(array) {
+  const mainArray = [];
+
+  for(let i = 0; i < array.length; i++) {
+    if(mainArray.includes(array[i]) === false) {
+      mainArray.push(array[i]);
+    }
+  }
+  return mainArray;
+}
