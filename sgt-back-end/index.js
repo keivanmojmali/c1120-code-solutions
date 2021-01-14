1e13ac23eed399c42ff0a213850f200ac27ecaf2
Keivan Mojmali <mojmalik@gmail.com>
2021-01-13 16:10:09 -0800

    working on put

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -8,7 +8,7 @@ const db = new pg.Pool({
 app.use(expressJson);
 
 
-app.get('/api/grades',(req,res,next)=>{
+app.get('/api/grades',(req,res)=>{
 const sql = `
 select *
 from "grades"
@@ -18,12 +18,37 @@ db.query(sql).then(result=>{
   const gradesArray = result.rows;
   res.status(200).json(gradesArray);
 }).catch(err => {
-  res.sendStatus(500);
+  res.status(500).json({'error': 'something went wrong'});
 })
 })
 
 
+app.post('/api/grades',(req,res)=>{
+  const content = req.body;
 
+  if(content === undefined) {
+    res.status(400).json({'error': 'Insert values are required'});
+    return;
+  }
+  if (content.course === undefined || content.name == undefined || Number.isInteger(content.score) === false) {
+      res.status(400).json({'error': 'Please double check input values'});
+    }
+  const sql = `
+  insert into "grades" ("course","gradeId","name","score")
+  values ('${content.course}','${content.gradeId}','${content.name}','${content.score}')
+  returning *
+  `;
+
+  db.query(sql).then(result => {
+    const returnRow = result.rows[0];
+    res.status(201).json(returnRow);
+  }).catch(err => {[
+    res.status(500).json({'error': 'something went wrong'})
+  ]})
+
+
+
+})
 
 
 
