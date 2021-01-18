65e6ebcb85e7c39d64a93cb3dc1e0187535f322b
Keivan Mojmali <mojmalik@gmail.com>
2021-01-18 15:15:29 -0800

    react-function-components | Keivan Mojmali

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const root = document.getElementById('root');
+
+function CustomButton(props) {
+  return <button> Click Me! </button>
+}
+
+ReactDOM.render(CustomButton(),root);
