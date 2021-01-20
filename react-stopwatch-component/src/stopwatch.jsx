974563ac4332aaef4337b00c3d4b9b62585960d9
Keivan Mojmali <mojmalik@gmail.com>
2021-01-19 21:18:28 -0800

    working on t

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,40 +3,34 @@ import React from 'react';
 export default class StopWatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { on: false };
+    this.state = { on: false, ticker: 0 };
     this.handleClick = this.handleClick.bind(this);
-    this.handleClickOn = this.handleClickOn.bind(this);
-    let count = 0;
+    let intervalId = null;
   };
   handleClick() {
-    let intervalId = null;
-    if(this.state.on === false){
-    this.setState({on:true})
-    intervalId = setInterval(this.count++,1000);
+    if (this.state.on === false) {
+      intervalId = setInterval(() => {
+        let newNum = this.state.ticker + 1;
+        this.setState({ on: true, ticker: newNum });
+      }, 1000);
     } else {
-      this.count = 0;
+      this.setState({ on: false, ticker: 0 });
       clearInterval(intervalId);
     }
   };
+
   render() {
+    let iconClass = "fas fa-play";
     if (this.state.on) {
-      return (
-          <div onClick={this.handleClick}>
-            <div className="circle">
-              <h1>{this.count}</h1>
-            </div>
-            <i className="fas fa-pause"></i>
-          </div>
-      )
-    } else {
-      return (
-        <div onClick={this.handleClick}>
-          <div className="circle">
-            <h1>{this.count}</h1>
-          </div>
-          <i className="fas fa-play"></i>
-        </div>
-      )
+      iconClass = "fas fa-pause"
     }
+    return (
+      <div onClick={this.handleClick}>
+        <div className="circle">
+          <h1>{this.count}</h1>
+        </div>
+        <i className={this.iconClass}></i>
+      </div>
+    )
   }
 }
