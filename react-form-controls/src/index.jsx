e5e86ba27c5581cf1dd5ee9e5636f96a028e0ac9
Keivan Mojmali <mojmalik@gmail.com>
2021-01-20 15:33:09 -0800

    react-form-controls | Keivan Mojmali

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -5,12 +5,27 @@ const $root = document.querySelector('#root');
 class NewsletterForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {email: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+  handleChange(event) {
+
+    this.setState({email: event.target.value});
+  };
+  handleSubmit(event) {
+    console.log('Value of State :', this.state)
+    event.preventDefault();
   }
+
+
+
+
 render() {
   return (
-    <form>
+    <form onSubmit={this.handleSubmit}>
       <label for="email">Email</label>
-      <input type="text" name="email"></input>
+      <input onChange={this.handleChange} type="text" name="email" value={this.state.value}></input>
       <button type="submit">Sign Up</button>
     </form>
   )
